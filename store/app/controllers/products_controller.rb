ad7369e04ae8b6cd2beddacfd5e7b23c62be59f2
illsism <raul.unknown@gmail.com>
2015-07-28 13:01:43 +0300

    First commit

---
+++ store/app/controllers/products_controller.rb
@@ -0,0 +1,33 @@
+class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_url
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description, :image_url, :price)
+  end
+end
---
+++ store/app/models/product.rb
@@ -0,0 +1,8 @@
+class Product < ActiveRecord::Base
+  validates :name, :description, :image_url, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :name, uniqueness: true, length: {minimum: 5,
+                                              message: "too short. Must be at least 5 characters long." }
+  validates :image_url, allow_blank:true, format: {with: %r{\.(gif|jpg|png)\Z}i,
+                                                   message: 'must be a URL for GIF, JPG or PNG image.' }
+end
