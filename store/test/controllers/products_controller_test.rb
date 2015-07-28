2ac40144a0b30d734de4b437b183d178d2841080
illsism <raul.unknown@gmail.com>
2015-07-28 16:25:39 +0300

    Create tests for new, edit, delete, show and make minor improvements

--- store/test/controllers/products_controller_test.rb
+++ store/test/controllers/products_controller_test.rb
@@ -5,8 +5,8 @@ class ProductsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-  test "should get show" do
-    get :show
+  test "should get index" do
+    get :index
     assert_response :success
   end
 
