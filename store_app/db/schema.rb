da17ba318476af7700f7ee73fdd4916e4fa2e082
ovidiuMaier <maier.ioan.ovidiu@gmail.com>
2015-07-31 12:00:19 +0300

    Add Remember me checkbox + more tests

--- store_app/db/schema.rb
+++ store_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729145644) do
+ActiveRecord::Schema.define(version: 20150730122135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20150729145644) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "picture"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
