0d1fea826638b72b0279fafbd6ff8c0fbc52fd98
ovidiuMaier <maier.ioan.ovidiu@gmail.com>
2015-08-05 15:56:35 +0300

    Fix tests

--- store_app/test/controllers/static_pages_controller_test.rb
+++ store_app/test/controllers/static_pages_controller_test.rb
@@ -4,25 +4,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Pawn Shop"
+    assert_select "title", "Antique Store"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Pawn Shop"
+    assert_select "title", "Help | Antique Store"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Pawn Shop"
+    assert_select "title", "About | Antique Store"
   end
 
   test "should get contact" do
    get :contact
    assert_response :success
-   assert_select "title", "Contact | Pawn Shop"
+   assert_select "title", "Contact | Antique Store"
  end
 
 end
